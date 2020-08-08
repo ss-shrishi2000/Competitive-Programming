53b59d81ce1d7177bc274ce2f55544b368faca7b
ss_shrishi2000 <65821403+ss-shrishi2000@users.noreply.github.com>
2020-08-08 13:37:46 +0530

    Add files via upload

---
+++ ABlendOfSpringtime.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;cin>>s;
+    int l=s.size();
+    for(int i=0;i<l-2;i++)
+    {
+        if(s.substr(i,3)=="BAC" || (s.substr(i,3)=="ABC") || (s.substr(i,3)=="CBA") || (s.substr(i,3)=="ACB") ||
+           (s.substr(i,3)=="BCA") || (s.substr(i,3)=="CAB")){cout<<"Yes";return 0;}
+    }
+    cout<<"No";
+    return 0;
+}
