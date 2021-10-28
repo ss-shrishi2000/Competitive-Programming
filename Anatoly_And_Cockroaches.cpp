ef951c78d550a67c97eaea1e1b2cc3e3aea1ee81
Srishi__1803 <65821403+ss-shrishi2000@users.noreply.github.com>
2021-10-29 00:19:53 +0530

    Update and rename AnatolyAndCockroaches.cpp to Anatoly_And_Cockroaches.cpp

---
+++ Anatoly_And_Cockroaches.cpp
@@ -0,0 +1,71 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int l;cin>>l;
+    string s;cin>>s;
+    int m=0,m1=0;
+    for(int i=l-1;i>1;i--)
+    {
+        if(s[i]=='r'&&(s[i-1]=='r'))
+        {
+            if(s[i-2]=='b')
+            {
+                swap(s[i-1],s[i-2]);
+                i-=2;
+                m++;
+            }
+            else
+            {
+                m++;
+                s[i-1]='b';
+            }
+        }
+       else if(s[i]=='b'&&(s[i-1]=='b'))
+       {
+            if(s[i-2]=='r')
+            {
+                swap(s[i-1],s[i-2]);
+                i-=2;
+                m++;
+            }
+            else
+            {
+                m++;
+                s[i-1]='r';
+            }
+        }
+    }
+    for(int i=0;i<l-3;i++)
+    {
+        if(s[i]=='r'&&(s[i+1]=='r'))
+        {
+            if(s[i+2]=='b')
+            {
+                swap(s[i+1],s[i+2]);
+                i+=2;
+                m1++;
+            }
+            else
+            {
+                m1++;
+                s[i+1]='b';
+            }
+        }
+       else if(s[i]=='b'&&(s[i+1]=='b'))
+       {
+            if(s[i-2]=='r')
+            {
+                swap(s[i+1],s[i+2]);
+                i+=2;
+                m1++;
+            }
+            else
+            {
+                m1++;
+                s[i+1]='r';
+            }
+        }
+    }
+    cout<<max(m1,m);
+}
