b05a394b545b4ead8a47dddd60a177b81f2d6484
Srishi__1803 <65821403+ss-shrishi2000@users.noreply.github.com>
2022-01-20 23:58:49 +0530

    Add files via upload

---
+++ Yuhao_And_Parenthesis.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n; cin>>n;
+    int cnt=0;
+    map <int, map<int,int>> mp ;
+    while (n--)
+    {
+        string s; cin>>s;
+        int left=0, right=0;
+
+        for(int i=0; i<s.size(); i++)
+        {
+            if (s[i] == ')')
+                if (right)
+                  right-- ;
+                else
+                left++ ;
+            else
+                right++ ;
+        }
+        if ( left && right)
+               continue;
+
+        if ( mp[left][right])
+        {
+            cnt++ ;
+            mp[left][right]-- ;
+        }
+        else
+            mp[right][left]++ ;
+    }
+    cout << cnt ;
+    return 0;
+
+}
