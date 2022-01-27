5b1a539dd418a6a5380223c412a55d5cafd86559
Srishi__1803 <65821403+ss-shrishi2000@users.noreply.github.com>
2022-01-27 23:29:07 +0530

    Update Business_Trip.cpp

--- Business_Trip.cpp
+++ Business_Trip.cpp
@@ -5,12 +5,19 @@ int main()
     int k;cin>>k;
     int a[12];
     long long s=0;
-    for(int i=0;i<12;i++){cin>>a[i];s+=a[i];}
+    for(int i=0;i<12;i++)
+    {
+        cin>>a[i];
+        s+=a[i];
+    }
     sort(a,a+12,greater<int>());
-    if(k==0)cout<<0;
+    if(k==0)
+        cout<<0;
     else{
-            if(k>s)cout<<-1;
-    else{
+            if(k>s)
+                cout<<-1;
+    else
+    {
         int i=0;
         while(k>0)
         {
