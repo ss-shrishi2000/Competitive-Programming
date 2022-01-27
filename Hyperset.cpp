e71e3b51376ea69bfb3ed5a36f6cf608fb5e86be
Srishi__1803 <65821403+ss-shrishi2000@users.noreply.github.com>
2022-01-27 23:28:16 +0530

    Update Hyperset.cpp

--- Hyperset.cpp
+++ Hyperset.cpp
@@ -3,13 +3,10 @@ using namespace std;
 typedef long long LL;
 
 char ch[5] = "EST";
-
 char orther(char a,char b)
 {
-	for(int i=0; i<3; i++)
-    {
-		if(ch[i] != a && ch[i] != b)
-        {
+	for(int i=0; i<3; i++){
+		if(ch[i] != a && ch[i] != b){
 			return ch[i];
 		}
 	}
@@ -21,18 +18,14 @@ int main()
 	int n,k,ans = 0;
 	string s;
 	set<string> st;
-
 	cin >> n >> k;
-	for(int i=1; i<=n; i++)
-    {
+	for(int i=1; i<=n; i++){
 		cin >> s;
 		for(auto it:st)
 		{
 			string t = "";
-			for(int j=0; j<k; j++)
-            {
-				if(s[j] == it[j])
-                {
+			for(int j=0; j<k; j++){
+				if(s[j] == it[j]){
 					t += s[j];
 				}
 				else
