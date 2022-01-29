31fa746afb4a137173e8cdae3207d1d129dcfa3e
Srishi__1803 <65821403+ss-shrishi2000@users.noreply.github.com>
2022-01-29 20:57:50 +0530

    Update and rename AlyonaAndNumbers.cpp to Alyona_And_Numbers.cpp

--- Alyona_And_Numbers.cpp
+++ Alyona_And_Numbers.cpp
@@ -2,10 +2,13 @@
 using namespace std;
 int main()
 {
-    int n,m;cin>>n>>m;
+    int n,m;
+    cin>>n>>m;
     long long int ans=0;
+    
     for(int i=1;i<=m;i++)
         ans+=(n+(i%5))/5;
+    
     cout<<ans;
     return 0;
 }
