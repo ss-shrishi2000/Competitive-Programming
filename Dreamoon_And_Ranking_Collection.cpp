e5483cbc402890e869ae46b0f16c391a86f90a19
Srishi__1803 <65821403+ss-shrishi2000@users.noreply.github.com>
2022-03-12 23:44:03 +0530

    Update and rename DreamoonAndRanlkingCollection.cpp to Dreamoon_And_Ranking_Collection.cpp

---
+++ Dreamoon_And_Ranking_Collection.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;cin>>t;
+    while(t--)
+    {
+    int n,x;
+    cin>>n>>x;
+    int a[n];
+    set<int> s;
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+        s.insert(a[i]);
+    }
+    int c=0,d=1;
+    while(true)
+    {
+    if(s.find(d)!=s.end())
+        c++;
+     else
+     {
+         if(x>0)
+         {
+             c++;
+             x--;
+         }
+   else 
+       break;
+     }
+    d++;
+    }
+    cout<<c<<endl;
+    }
+}
