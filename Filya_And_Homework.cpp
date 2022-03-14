cac7ba622b519fac6a34342be0918c976ec3155a
Srishi__1803 <65821403+ss-shrishi2000@users.noreply.github.com>
2022-03-14 23:50:13 +0530

    Update and rename FilyaAndHomework.cpp to Filya_And_Homework.cpp

--- Filya_And_Homework.cpp
+++ Filya_And_Homework.cpp
@@ -2,17 +2,19 @@
 using namespace std;
 int main()
 {
-    set<int> s;
-	int n,x;cin >> n;
+        set<int> s;
+	int n,x;
+	cin >> n;
 	for (int i = 0; i < n; i++)
-    {
-		cin >> x;s.insert(x);
+	{
+		cin >> x;
+		s.insert(x);
 	}
 	bool f=false;
 	if (s.size() <= 2)
         f= true;
 	else if (s.size() == 3)
-    {
+	{
 		int a = *s.begin(), b = *s.rbegin();
 		if (((b - a) % 2 == 0) && (s.find((a + b) / 2) != s.end()))
 		                 f=true;
@@ -21,6 +23,5 @@ int main()
 	cout << "YES";
 	else
 	cout << "NO";
-
 	return 0;
 }
