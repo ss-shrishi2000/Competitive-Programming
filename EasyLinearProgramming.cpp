655c432a462b7a02e2b86a11b290531911d5dbd4
Srishi__1803 <65821403+ss-shrishi2000@users.noreply.github.com>
2022-03-14 23:44:20 +0530

    Update EasyLinearProgramming.cpp

--- EasyLinearProgramming.cpp
+++ EasyLinearProgramming.cpp
@@ -2,9 +2,14 @@
 using namespace std;
 int main()
 {
-    int a, b, c, k; cin >> a >> b >> c >> k;
-if(k <= a) cout << k << endl;
- else if(k <= a + b) cout << a << endl;
- else cout << a - (k - a - b) << endl;
+    int a, b, c, k;
+    cin >> a >> b >> c >> k;
+    if(k <= a)
+        cout << k << endl;
+ else if(k <= a + b)
+     cout << a << endl;
+ else
+     cout << a - (k - a - b) << endl;
+    
  return 0;
 }
