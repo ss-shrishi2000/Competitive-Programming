8eadfb67c261bb0f3b3ea3745f20c2a4677d6661
Srishi__1803 <65821403+ss-shrishi2000@users.noreply.github.com>
2022-03-15 23:19:01 +0530

    Add files via upload

--- Barnicle.cpp
+++ Barnicle.cpp
@@ -2,21 +2,75 @@
 using namespace std;
 int main()
 {
-    string s;cin>>s;
-    int p=stoi(s.substr(s.find("e")+1));
-    if(p==0 || s[0]!='0')cout<<s[0];
-    int j=2;
-    for(;j<s.size() and p>0 and s[j]!='e';j++,p--)
+    string s;
+    cin>>s;
+    int power=0, dot_pos=0;
+
+    int i=s.size()-1;
+    string prt="";
+    while(i>=0 && s[i]!='e')
+        { prt+= s[i]; i--; }
+
+    reverse(prt.begin(),prt.end());
+
+    for(int i=0; i<prt.size(); i++)
+        power = power*10 + (prt[i]-'0');
+
+    dot_pos=i;
+    i-=1;
+
+    string p="";
+    while(i>=0)
     {
-        cout<<s[j];
+        if(s[i]!='.')
+            p+=s[i];
+
+        i--;
     }
-    if(s[j]!='e')
+    reverse(p.begin(),p.end());
+    dot_pos = s.size() - 1 - dot_pos;
+
+    if(p==string(p.size(),'0'))
     {
-        if(!(s[j]=='0' && s[j+1]=='e'))
+        cout<<0;
+        return 0;
+    }
+
+    if(dot_pos + power >= p.size()+1)
+    {
+        //cout<<1<<endl;
+        cout<<p;
+        for(int h = 1; h <= power - p.size()+1 ; h++)
+            cout<<0;
+    }
+    else
+    {
+        //cout<<2<<endl;
+        int j=-1;
+        string ans="";
+        bool f=false;
+        for(int k=0; k<p.size(); k++)
         {
-            cout<<'.';
-            for(;s[j]!='e';j++)cout<<s[j];
+            if(j==power)
+                ans+=".";
+
+            ans+=p[k];
+            j++;
+        }
+        string ex="";
+        j=ans.size()-1;
+        while(j>=0 && ans[j]=='0')
+        {
+            j--;
+        }
+        if(j==ans.size()-1 && ans[j]!='0')
+        {
+            cout<<ans;
+        }
+        else{
+            ans.erase(j,s.size()-1);
+        cout<<ans<<endl;
         }
     }
-    while(p>0){cout<<0;p--;}
+    return 0;
 }
