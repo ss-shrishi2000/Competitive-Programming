41e9291455d6b94748a098a5a555fd4be49dfca2
Srishi__1803 <65821403+ss-shrishi2000@users.noreply.github.com>
2022-03-15 23:18:25 +0530

    Add files via upload

--- Common_Divisors.cpp
+++ Common_Divisors.cpp
@@ -1,37 +1,30 @@
 #include<bits/stdc++.h>
 using namespace std;
-string a,b;
-int x,y,ans=0;
-int check(int s)
+
+int valid(string s, int x)
 {
-    for(int i=0;i<x;i++)
+    for (int i = 0; i < s.size(); i++)
     {
-        if(a[i]!=a[i%s])
-            return 0;
-    }
-    for(int i=0;i<y;i++)
-    {
-        if(b[i]!=a[i%s])
+        if (s[i] != s[i % x])
             return 0;
     }
     return 1;
 }
-
 int main()
 {
-   cin>>a>>b;
-   x=a.size();
-   y=b.size();
-   int g=__gcd(x,y);
-   for(int i=1;i*i<=g;i++)
-   {
-       if(g%i==0)
-       {
-          ans+=check(i);
-          if(i*i!=g)
-            ans+=check(g/i);
-       }
-   }
-   cout<<ans<<endl;
-   return 0;
+    string s1, s2; cin >> s1 >> s2;
+    int n = s1.size(), m = s2.size();
+    int x = min(s1.size(), s2.size());
+    int cnt=0;
+    for (int i = 1; i <= x; i++)
+    {
+        if (n % i == 0 and m % i == 0)
+        {
+            if (s1.substr(0, i) == s2.substr(0, i) and valid(s1, i) and valid(s2, i))
+                cnt++;
+        }
+    }
+    cout << cnt;
+
+    return 0;
 }
