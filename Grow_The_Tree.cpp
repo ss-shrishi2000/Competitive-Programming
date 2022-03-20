548b9e41fbbcb151d5a7c4042f8bec72024960ca
Srishi__1803 <65821403+ss-shrishi2000@users.noreply.github.com>
2022-03-20 17:47:07 +0530

    Update and rename GrowTheTree.cpp to Grow_The_Tree.cpp

--- Grow_The_Tree.cpp
+++ Grow_The_Tree.cpp
@@ -2,12 +2,19 @@
 using namespace std;
 int main()
 {
-    int n;cin>>n;
+    int n;
+    cin>>n;
     long long int a[n];
     long long int s=0,k=0;
-    for(int i=0;i<n;i++){cin>>a[i];s+=a[i];}
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+        s+=a[i];
+    }
     sort(a,a+n);
-    for(int i=0;i<n/2;i++)k+=a[i];
+    for(int i=0;i<n/2;i++)
+        k+=a[i];
+    
     s-=k;
     long long int p=s*s+k*k;
     cout<<p;
