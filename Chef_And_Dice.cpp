4ad6c1a0e2657f716ae563641582a427ef913e6f
Srishi__1803 <65821403+ss-shrishi2000@users.noreply.github.com>
2022-03-22 20:00:50 +0530

    Update and rename ChefAndDice.cpp to Chef_And_Dice.cpp

--- Chef_And_Dice.cpp
+++ Chef_And_Dice.cpp
@@ -5,7 +5,8 @@ int main()
     int t;cin>>t;
     while(t--)
     {
-        long long n;cin>>n;
+        long long n;
+        cin>>n;
         long long s=0;
         s= (n/4)*44;
         long long r=n%4;
