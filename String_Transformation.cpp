5c8050e174078d3c249e6901d94829bc7b22d200
Srishi__1803 <65821403+ss-shrishi2000@users.noreply.github.com>
2022-04-02 00:37:13 +0530

    Update and rename StringTransformation.cpp to String_Transformation.cpp

--- String_Transformation.cpp
+++ String_Transformation.cpp
@@ -2,19 +2,24 @@
 using namespace std;
 int main()
 {
-    string s;cin>>s;
-    if(s.size()<26)cout<<"-1";
-    else{
+    string s;
+	cin>>s;
+    if(s.size()<26)
+	    cout<<"-1";
+    else
+    {
     int j=0;
     for(int i=0;i<s.size();i++)
     {
-        if(s[i]-'a'<=j){
+        if(s[i]-'a'<=j)
+	{
             s[i]='a'+(j++);
         }
-        if (j == 26) {
-			cout<<s<<endl;
-			return 0;
-		}
+        if (j == 26)
+	{	
+		cout<<s<<endl;
+		return 0;
+	}
     }
     cout<<"-1";
     }
