eae38d006aec3cab91d1234747d08f7e6e4e2af0
Srishi__1803 <65821403+ss-shrishi2000@users.noreply.github.com>
2022-04-07 22:30:51 +0530

    Update and rename DigitGame.cpp to Digit_Game.cpp

--- Digit_Game.cpp
+++ Digit_Game.cpp
@@ -5,8 +5,10 @@ int main()
     int t;cin>>t;
     while(t--)
     {
-        int n;cin>>n;
-        string s;cin>>s;
+        int n;
+	cin>>n;
+        string s;
+	cin>>s;
         int e=0,o=0;
         for(int i=0;i<n;i++)
         {
@@ -15,7 +17,8 @@ int main()
             else if((s[i]-'0')%2!=0 && (i%2==0))
                 o++;
         }
-        if(((n%2==0)&&e) || (n&1 && o==0))cout <<2<<endl;
+        if(((n%2==0)&&e) || (n&1 && o==0))
+		cout <<2<<endl;
 	    else cout <<1<<endl;
     }
 }
