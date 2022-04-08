8f7a6377328e9ad84f96aeda6fad27ad156b5ea9
Srishi__1803 <65821403+ss-shrishi2000@users.noreply.github.com>
2022-04-08 23:19:40 +0530

    Update and rename DivisibilytDifferences.cpp to Divisibily_Differences.cpp

--- Divisibily_Differences.cpp
+++ Divisibily_Differences.cpp
@@ -2,26 +2,33 @@
 using namespace std;
 int main()
 {
-    long long n,k,m;cin>>n>>k>>m;
+    long long n,k,m;
+    cin>>n>>k>>m;
     vector<long long> rem[m];
     long long a[n];
-    for(int i=0;i<n;i++)cin>>a[i];
+    for(int i=0;i<n;i++)
+        cin>>a[i];
     for(int i=0;i<n;i++)
     {
         int x=a[i]%m;
         rem[x].push_back(a[i]);
     }
+    
     bool f=false;
+    
     for(int i=0;i<m;i++)
     {
         if(rem[i].size()>=k)
         {
             f=true;
             cout<<"Yes"<<endl;
-            for(int j=0;j<k;j++)cout<<rem[i][j]<<" ";
+            for(int j=0;j<k;j++)
+                cout<<rem[i][j]<<" ";
         }
-        if(f==true)break;
+        if(f==true)
+            break;
     }
-    if(f==false)cout<<"No";
+    if(f==false)
+        cout<<"No";
     return 0;
 }
