5f04ed0234e78de5c3ee4097134f6876649a1ac2
Srishi__1803 <65821403+ss-shrishi2000@users.noreply.github.com>
2022-04-10 19:34:49 +0530

    Update and rename ABlendOfSpringtime.cpp to A_Blend_Of_Springtime.cpp

--- A_Blend_Of_Springtime.cpp
+++ A_Blend_Of_Springtime.cpp
@@ -7,7 +7,11 @@ int main()
     for(int i=0;i<l-2;i++)
     {
         if(s.substr(i,3)=="BAC" || (s.substr(i,3)=="ABC") || (s.substr(i,3)=="CBA") || (s.substr(i,3)=="ACB") ||
-           (s.substr(i,3)=="BCA") || (s.substr(i,3)=="CAB")){cout<<"Yes";return 0;}
+           (s.substr(i,3)=="BCA") || (s.substr(i,3)=="CAB"))
+        {
+            cout<<"Yes";
+            return 0;
+        }
     }
     cout<<"No";
     return 0;
