8cebd8c9a450fc8051c2963d205e72942869a2ed
Srishi__1803 <65821403+ss-shrishi2000@users.noreply.github.com>
2022-04-11 23:33:25 +0530

    Update and rename 2048Game.cpp to 2048_Game.cpp

--- 2048_Game.cpp
+++ 2048_Game.cpp
@@ -8,13 +8,18 @@ int main()
         int n;cin>>n;
         int a[n];
         vector<int> b;
-        for(int i=0;i<n;i++)cin>>a[i];
+        for(int i=0;i<n;i++)
+            cin>>a[i];
+        
         for(int i=0;i<n;i++)
         {
-            if(a[i]<=2048)b.push_back(a[i]);
+            if(a[i]<=2048)
+                b.push_back(a[i]);
         }
         long long int sum=accumulate(b.begin(),b.end(),0LL);
-        if(sum>=2048)cout<<"YES"<<endl;
-        else cout<<"NO"<<endl;
+        if(sum>=2048)
+            cout<<"YES"<<endl;
+        else 
+            cout<<"NO"<<endl;
     }
 }
