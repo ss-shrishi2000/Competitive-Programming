8c50b3f904f80cca52b2ff037d1dd872aae87c5f
Srishi__1803 <65821403+ss-shrishi2000@users.noreply.github.com>
2022-04-15 20:31:26 +0530

    Update and rename AvoidingZero.cpp to Avoiding_Zero.cpp

--- Avoiding_Zero.cpp
+++ Avoiding_Zero.cpp
@@ -2,13 +2,17 @@
 using namespace std;
 int main()
 {
-    int t;cin>>t;
+    int t;
+    cin>>t;
     while(t--)
     {
-        int n;cin>>n;
+        int n;
+        cin>>n;
         int a[n];
         long long int s=0,s2=0;
-        for(int i=0;i<n;i++)cin>>a[i],s+=a[i];
+        for(int i=0;i<n;i++)
+            cin>>a[i],s+=a[i];
+        
         if(s==0)
         {
             cout<<"NO"<<endl;
@@ -18,9 +22,11 @@ int main()
         bool f=true;
         cout<<"YES"<<endl;
         if(s<0)
-            for(int i=0;i<n;i++)cout<<a[i]<<" ";
+            for(int i=0;i<n;i++)
+                cout<<a[i]<<" ";
         else
-            for(int i=n-1;i>=0;i--)cout<<a[i]<<" ";
+            for(int i=n-1;i>=0;i--)
+                cout<<a[i]<<" ";
             cout<<endl;
      }
 }
