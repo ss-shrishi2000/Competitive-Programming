0f710d9ff530768140074586644cd9abf5c5b72e
Srishi__1803 <65821403+ss-shrishi2000@users.noreply.github.com>
2022-04-15 20:32:35 +0530

    Update EasyAndHard.cpp

--- EasyAndHard.cpp
+++ EasyAndHard.cpp
@@ -2,14 +2,16 @@
 using namespace std;
 int main()
 {
-    int n;cin>>n;
+    int n;
+    cin>>n;
     int a[2]={0},x;
     for(int i=0;i<n;i++)
     {
-        cin>>x;
-        a[x]++;
+        cin>>x; a[x]++;
     }
-    if(a[1]==0)cout<<"EASY";
-    else cout<<"HARD";
+    if(a[1]==0)
+        cout<<"EASY";
+    else 
+        cout<<"HARD";
     return 0;
 }
