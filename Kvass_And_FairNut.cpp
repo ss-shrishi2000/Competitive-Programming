9baa292f0da0d508f4db36a8c7480625727d8277
Srishi__1803 <65821403+ss-shrishi2000@users.noreply.github.com>
2022-04-17 23:50:07 +0530

    Update and rename KvassAndFairNut.cpp to Kvass_And_FairNut.cpp

--- Kvass_And_FairNut.cpp
+++ Kvass_And_FairNut.cpp
@@ -5,11 +5,7 @@ int main()
     long long int s,k=0,sum=0;
     int n;cin>>n>>s;
     int a[n];
-    for(int i=0;i<n;i++)
-	{
-		cin>>a[i];
-		sum+=a[i];
-	}
+    for(int i=0;i<n;i++){cin>>a[i];sum+=a[i];}
 	long long int m=*min_element(a,a+n);
 	long long ans=sum-s;
 	if(ans>=0)
