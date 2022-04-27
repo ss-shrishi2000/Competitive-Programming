1d1167fc8933abab4dfedc1667bfe450b4b4934b
Srishi__1803 <65821403+ss-shrishi2000@users.noreply.github.com>
2022-04-27 23:48:29 +0530

    Add files via upload

---
+++ GCD_vs_LCM.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        long long int n; cin>>n;
+        cout<< 1 << " " << n-3 << " " << 1 << " " << 1 << endl;
+    }
+    return 0;
+}
