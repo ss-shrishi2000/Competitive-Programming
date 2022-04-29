9ca3d74b52ca7aeb3afd758df4f5efd985104376
Srishi__1803 <65821403+ss-shrishi2000@users.noreply.github.com>
2022-04-29 23:51:34 +0530

    Update and rename CharacterSwap.cpp to Character_Swap.cpp

--- Character_Swap.cpp
+++ Character_Swap.cpp
@@ -7,9 +7,12 @@ int main()
     while(t--)
     {
         vector<int> v;
-        int n;cin>>n;
-        string a,b;cin>>a>>b;
-        for(int i=0;i<n;i++) if(a[i]!=b[i]) v.push_back(i);
+        int n;
+        cin>>n;
+        string a,b;
+        cin>>a>>b;
+        for(int i=0;i<n;i++) 
+            if(a[i]!=b[i]) v.push_back(i);
         if(v.size()==2 && (a[v[0]]==a[v[1]]) &&(b[v[0]]==b[v[1]])) cout<<"YES"<<endl;
         else cout<<"NO"<<endl;
         v.clear();
