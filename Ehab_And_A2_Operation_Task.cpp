7a5c905bf7fa607b71f3ab46dbd1894cad70df46
Srishi__1803 <65821403+ss-shrishi2000@users.noreply.github.com>
2022-05-09 21:17:09 +0530

    Update and rename EhabAndA2operationtask.cpp to Ehab_And_A2_Operation_Task.cpp

--- Ehab_And_A2_Operation_Task.cpp
+++ Ehab_And_A2_Operation_Task.cpp
@@ -2,9 +2,13 @@
 using namespace std;
 int main()
 {
-    int n;cin>>n;
+    int n;
+    cin>>n;
     int a[n+1];
-    for(int i=0;i<n;i++){cin>>a[i];}
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
     cout<<n+1<<endl;
      cout<<"1 "<<n<<" 10000"<<endl;
     for(int i=0; i<n; i++)
