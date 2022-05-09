484d5a77d0a202fc02af496835d732c7f1a98a45
Srishi__1803 <65821403+ss-shrishi2000@users.noreply.github.com>
2022-05-09 21:07:52 +0530

    Update and rename DivideCandies.cpp to Divide_Candies.cpp

--- Divide_Candies.cpp
+++ Divide_Candies.cpp
@@ -5,11 +5,12 @@ int main()
     long long int n,m;
     cin>>n>>m;
     long long int cnt=0;
-	for(int i=1;i<=m;++i)
+    for(int i=1;i<=m;++i)
     {
     for(int j=1;j<=m;++j)
     {
-        if((i*i+j*j)%m==0)cnt+=((n-i+m)/m)*((n-j+m)/m);
+        if((i*i+j*j)%m==0)
+		cnt+=((n-i+m)/m)*((n-j+m)/m);
     }
     }
     cout<<cnt<<endl;
