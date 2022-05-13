87d1f6e9110a3a0746b10e372c22c03aa27340ec
Srishi__1803 <65821403+ss-shrishi2000@users.noreply.github.com>
2022-05-13 21:45:52 +0530

    Update and rename AlmostRegularBracketsequence.cpp to Almost_Regular_Bracket_Sequence.cpp

--- Almost_Regular_Bracket_Sequence.cpp
+++ Almost_Regular_Bracket_Sequence.cpp
@@ -3,25 +3,34 @@ using namespace std;
 int main()
 {
     int n;cin>>n;
-    string s;cin>>s;
-    if(n%2!=0)cout<<"0";
-    else{
+    string s;
+    cin>>s;
+    if(n%2!=0)
+        cout<<"0";
+    else
+    {
     bool f=true;
     int c=0,p=0;
     for(int i=0;i<n;i++)
     {
-        c=0;
-      if(s[i]=='(')s[i]=')';
-      else s[i]='(';
+      c=0;
+      if(s[i]=='(')
+          s[i]=')';
+      else 
+          s[i]='(';
+        
       for(int j=i+1;j<n;j++)
       {
           if(s[j]=='(')c++;
           else c--;
           if(c<0){break;}
       }
-      if(c==0)p++;
-      if(s[i]=='(')s[i]=')';
-      else s[i]='(';
+      if(c==0)
+          p++;
+      if(s[i]=='(')
+          s[i]=')';
+      else 
+          s[i]='(';
     }
     cout<<p<<endl;
     }
