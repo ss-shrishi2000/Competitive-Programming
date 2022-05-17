69b7d80f1e152fc427f72065ef70343e0804119a
Srishi__1803 <65821403+ss-shrishi2000@users.noreply.github.com>
2022-05-17 23:45:54 +0530

    Update and rename NewYearAndNorthPole.cpp to New_Year_And_North_Pole.cpp

--- New_Year_And_North_Pole.cpp
+++ New_Year_And_North_Pole.cpp
@@ -2,12 +2,17 @@
 using namespace std;
 int main()
 {
-    int d,tot=0,n;cin>>n;
+    int d,tot=0,n;
+    cin>>n;
     string s;
     for(int i=0;i<n;i++)
     {
         cin>>d>>s;
-        if(tot>20000 or tot<0){cout<<"NO";return 0;}
+        if(tot>20000 or tot<0)
+        {
+            cout<<"NO";
+            return 0;
+        }
         if(i==0)
         {
             if(s!="South"){cout<<"NO";return 0;}              //can move only to south because it is at north pole
