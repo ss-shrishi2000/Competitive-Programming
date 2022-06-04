83989e83a72201ed331fa8824ad4220619960851
Srishi__1803 <65821403+ss-shrishi2000@users.noreply.github.com>
2022-06-04 23:06:50 +0530

    Update and rename 01-Game.cpp to 01_Game.cpp

--- 01_Game.cpp
+++ 01_Game.cpp
@@ -5,15 +5,18 @@ int main()
     int n;cin>>n;
     while(n--)
     {
-        string s;cin>>s;
+        string s;
+        cin>>s;
         int c0=0,c1=0;
         for(int i=0;i<s.size();i++)
         {
             if(s[i]=='0')c0++;
             else c1++;
         }
-        int m=min(c0,c1);
-        if(m%2==0)
+
+        int m1=min(c0,c1);
+
+        if(m1%2==0)
            cout<<"NET"<<endl;
         else  
            cout<<"DA"<<endl;
