ab01ede24fda858f62f72981ffee866e618981cd
Srishi__1803 <65821403+ss-shrishi2000@users.noreply.github.com>
2022-06-09 23:32:45 +0530

    Update and rename GameWithTheArray.cpp to Game_With_The_Array.cpp

--- Game_With_The_Array.cpp
+++ Game_With_The_Array.cpp
@@ -2,9 +2,12 @@
 using namespace std;
 int main()
 {
-    int n,s;cin>>n>>s;
-    if(s<2*n)cout<<"NO"<<endl;
-    else{
+    int n,s;
+    cin>>n>>s;
+    if(s<2*n)
+      cout<<"NO"<<endl;
+    else
+    {
         cout<<"YES"<<endl;
         for(int i=0;i<n-1;i++)
         {
