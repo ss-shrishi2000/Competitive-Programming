6bc1538bb7aea096eab8001a20e81deea58ea087
Srishi__1803 <65821403+ss-shrishi2000@users.noreply.github.com>
2022-06-12 22:49:58 +0530

    Add files via upload

--- Ternary_Sequence.cpp
+++ Ternary_Sequence.cpp
@@ -1,23 +1,43 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define ll long long
 int main()
 {
     int t;
     cin>>t;
     while(t--)
     {
-        ll x1,y1,z1,x2,y2,z2;
-		ll res=0;
-		cin>>x1>>y1>>z1;
-		cin>>x2>>y2>>z2;
-		res = 2*(min(z1,y2));
-		ll temp = min(z1,y2);
-		z1-=temp;
-		y2-=temp;
-		if(z2>z1+x1)
-			res-=2*(z2-(z1+x1));
-		cout<<res<<endl;
+        int x1, y1, z1, x2, y2, z2;
+        cin>>x1>>y1>>z1;
+        cin>>x2>>y2>>z2;
+        int s = x1+y1+z1;
+        //cout<<s<<endl;
+        long ans=0, p;
+
+        p = min(z1, y2);
+        ans += 2*p, z1-=p, y2-=p;
+
+        p = min(y1, x2);
+        y1-=p, x2-=p;
+
+        p = min(z1, x2);
+        z1-=p, x2-=p;
+
+        p = min(z1, z2);
+        z1-=p, z2-=p;
+
+        p = min(y1, y2);
+        y1-=p, y2-=p;
+
+        p = min(x1, y2);
+        x1-=p, y2-=p;
+
+        p = min(x1, z2);
+        x1-=p, z2-=p;
+
+        p = min(y1, z2);
+        ans-= 2*p, y1-=p, z2-=p;
+
+        cout<<ans<<endl;
     }
     return 0;
 }
