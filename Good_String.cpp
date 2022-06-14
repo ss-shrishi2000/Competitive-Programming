07e57e002a1144f8994f008eb20c136694085665
Srishi__1803 <65821403+ss-shrishi2000@users.noreply.github.com>
2022-06-14 22:45:19 +0530

    Add files via upload

--- Good_String.cpp
+++ Good_String.cpp
@@ -2,35 +2,37 @@
 using namespace std;
 int main()
 {
-  int n;cin>>n;
-  while(n--)
-  {
-      string s;cin>>s;
-      char a,b;
-      int ans=0;
-      for(a='0';a<='9';a++)
-      {
-          for(b='0';b<='9';b++)
-          {
-              int p=0,c=0;
-              for(int i=0;i<s.size();i++)
-              {
-                  if(s[i]==a and p==0)
-                  {
-                      c++;p=1-p;
-                  }
-                  else if(s[i]==b and p==1)
-                  {
-                      c++;p=1-p;
-                  }
-              }
-              if(a==b)ans=max(ans,c);
-              else {
-                if(c%2==1)ans=max(ans,c-1);
-                else ans=max(ans,c);
-              }
-          }
-      }
-      cout<<s.size()-ans<<endl;
-  }
+    int n; cin>>n;
+    string s; cin>>s;
+    stack<char> st;
+    st.push(s[0]);
+    for(int i=1; i<n; i++)
+    {
+        if(st.size()%2!=0 && st.top()==s[i])
+        {
+            continue;
+        }
+        else if(st.size()%2!=0 && st.top()!=s[i])
+        {
+            st.push(s[i]);
+        }
+        else if(st.size()%2==0)
+        {
+            st.push(s[i]);
+        }
+    }
+    string ans="";
+    while(st.empty()==false)
+    {
+        ans+=st.top();
+        st.pop();
+    }
+
+    reverse(ans.begin(), ans.end());
+    if(ans.size()%2)
+        ans.pop_back();
+
+    cout<<n-ans.size()<<endl;
+    cout<<ans<<endl;
+    return 0;
 }
