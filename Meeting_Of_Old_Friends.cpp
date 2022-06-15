7d431db907558fb98cc5d749311380fc0eb868e1
Srishi__1803 <65821403+ss-shrishi2000@users.noreply.github.com>
2022-06-15 23:15:47 +0530

    Update and rename MeetingOfOldFriends.cpp to Meeting_Of_Old_Friends.cpp

--- Meeting_Of_Old_Friends.cpp
+++ Meeting_Of_Old_Friends.cpp
@@ -2,7 +2,8 @@
 using namespace std;
 int main()
 {
-    long long int l1,r1,l2,r2,k;cin>>l1>>r1>>l2>>r2>>k;
+    long long int l1,r1,l2,r2,k;
+    cin>>l1>>r1>>l2>>r2>>k;
     long long int f=0,ans;
     long long int l=max(l1,l2);
     long long int r=min(r1,r2);
