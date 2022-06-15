916e24f3686e4330367a53af602cca0e24717938
Srishi__1803 <65821403+ss-shrishi2000@users.noreply.github.com>
2022-06-15 23:18:27 +0530

    Update and rename NewYearAndHurry.cpp to New_Year_And_Hurry.cpp

--- New_Year_And_Hurry.cpp
+++ New_Year_And_Hurry.cpp
@@ -2,14 +2,16 @@
 using namespace std;
 int main()
 {
-    int n,k;cin>>n>>k;
+    int n,k;
+    cin>>n>>k;
     int rem=240-k;
     for(int i=1;i<=n;i++)
     {
         rem-=i*5;
         if(rem<0)
         {
-            cout<<i-1;return 0;
+            cout<<i-1;
+            return 0;
         }
     }
     cout<<n<<endl;
