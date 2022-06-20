62a7d0b44d5e3413e7d18412e3a1086bbe15e637
Srishi__1803 <65821403+ss-shrishi2000@users.noreply.github.com>
2022-06-20 23:51:14 +0530

    Add files via upload

--- Equalize_The_Array.cpp
+++ Equalize_The_Array.cpp
@@ -2,25 +2,38 @@
 using namespace std;
 int main()
 {
-    int t;cin>>t;
+    int t;
+    cin>>t;
     while(t--)
     {
-        int n;
-        cin>>n;
+        int n; cin>>n;
         int a[n];
-        map<long long int,long long int> mp , freq;
-        for(int i=0;i<n;i++)
+        set<int> st;
+        map<int, int> mp;
+        for(int i=0; i<n; i++)
         {
             cin>>a[i];
             mp[a[i]]++;
-            freq[mp[a[i]]]++;
         }
-        long long ans=INT_MAX,a1=0;
-        for(auto it:freq)
-        {
-            a1 = max((it.first) * (it.second) , a1);
-        }
-        cout<< n-a1 <<endl;
+         long long int r = INT_MAX;
+
+         for(auto it = mp.begin(); it!=mp.end(); it++)
+            st.insert(it->second);
+
+         for(auto it=st.begin(); it!=st.end(); it++)
+         {
+             long long int c = 0;
+             for(auto i = mp.begin(); i!=mp.end(); i++)
+             {
+                 int f = i->second;
+                 if(f<(*it))
+                    c+=f;
+                 else
+                    c+=(f-(*it));
+             }
+             r = min(r, c);
+         }
+         cout<<r<<endl;
     }
     return 0;
 }
