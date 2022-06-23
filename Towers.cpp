2662826600b286f3164d82cd4637f7d026a50700
Srishi__1803 <65821403+ss-shrishi2000@users.noreply.github.com>
2022-06-23 22:42:45 +0530

    Add files via upload

--- Towers.cpp
+++ Towers.cpp
@@ -1,25 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
+#define pii pair<int, int>
 int main()
 {
-    int n,k;cin>>n>>k;
+    int n,k;
+    cin>>n>>k;
     int a[n];
-    for(int i=0;i<n;i++)cin>>a[i];
-    vector<pair<int,int> > ans;
-    int cnt=0;
+    pii pr[n];
+
+    for(int i=0; i<n; i++)
+        cin>>a[i], pr[i] = {a[i], i};
+
+    sort(pr, pr+n);
+
+    vector<pii> ans;
     while(k--)
     {
-       auto it1=min_element(a , a+n);
-       auto it2=max_element(a , a+n);
-       if(*it1==*it2)
-        break;
-       cnt++;
-       ans.push_back({it1-a , it2-a });
-       a[it1-a]++;
-       a[it2-a]--;
+       if(pr[0].first == pr[n-1].first)
+            break;
+
+       pr[0].first+=1;
+       pr[n-1].first-=1;
+       ans.push_back({pr[0].second, pr[n-1].second});
+       sort(pr, pr+n);
     }
-    cout << *max_element(a,a+n) - *min_element(a,a+n)<< " " << cnt << endl;
-    for(auto it:ans)
-        cout<<it.second+1<<" "<<it.first+1 << endl;
+    int x = pr[n-1].first - pr[0].first;
+    cout<<x<<" "<< ans.size()<<endl;
+
+    for(int i=0; i<ans.size(); i++)
+        cout<<ans[i].second+1 << " " << ans[i].first+1 << endl;
     return 0;
 }
