1ef263d22ebbb8b85490a401b091a22662f08763
Srishi__1803 <65821403+ss-shrishi2000@users.noreply.github.com>
2022-06-26 22:51:22 +0530

    Update CheckTheString.cpp

--- CheckTheString.cpp
+++ CheckTheString.cpp
@@ -6,24 +6,36 @@ int main()
     int a1=0,a2=0,a3=0;
     for(int i=0;i<s.size();i++)
     {
-        if(s[i]=='a'){a1++;p+=s[i];}
-        else if(s[i]=='b'){a2++;q+=s[i];}
+        if(s[i]=='a')
+        {
+            a1++;
+            p+=s[i];
+        }
+        else if(s[i]=='b')
+        {
+            a2++;
+            q+=s[i];
+        }
         else
         {
         a3++;
         r+=s[i];
         }
     }
-    if(a3==0 or a2==0 or a1==0){
-        cout<<"NO";
-        return 0;
+    if(a3==0 or a2==0 or a1==0)
+    {
+        cout<<"NO"; return 0;
     }
     if(a3==a1 or a3==a2)
     {
         p+=q+r;
-        if(p==s)cout<<"YES";
-        else cout<<"NO";
+        if(p==s)
+            cout<<"YES";
+        else 
+            cout<<"NO";
     }
-    else cout<<"NO";
+    else 
+        cout<<"NO";
+    
     return 0;
 }
