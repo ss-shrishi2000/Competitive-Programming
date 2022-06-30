4fb1ee7a36ab6af7249b053bf88358033e0f5b50
Srishi__1803 <65821403+ss-shrishi2000@users.noreply.github.com>
2022-06-30 23:14:39 +0530

    Update and rename GravityFlip.cpp to Gravity_Flip.cpp

--- Gravity_Flip.cpp
+++ Gravity_Flip.cpp
@@ -4,8 +4,10 @@ int main()
 {
     int n;cin>>n;
     int a[n];
-    for(int i=0;i<n;i++)cin>>a[i];
+    for(int i=0;i<n;i++)
+        cin>>a[i];
     sort(a,a+n);
-    for(int i=0;i<n;i++)cout<<a[i]<<" ";
+    for(int i=0;i<n;i++)
+        cout<<a[i]<<" ";
     return 0;
 }
