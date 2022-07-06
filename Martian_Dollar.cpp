349a7b0274176de196d3bb88536cbb833b6b5a02
Srishi__1803 <65821403+ss-shrishi2000@users.noreply.github.com>
2022-07-06 23:46:50 +0530

    Update and rename MartianDollar.cpp to Martian_Dollar.cpp

--- Martian_Dollar.cpp
+++ Martian_Dollar.cpp
@@ -2,17 +2,22 @@
 using namespace std;
 int main()
 {
-    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    int n,b;cin>>n>>b;
+    ios_base::sync_with_stdio(false); 
+    cin.tie(NULL); 
+    cout.tie(NULL);
+    int n,b;
+    cin>>n>>b;
     int a[n+1];
-    for(int i=0;i<n;i++){cin>>a[i];}
+    for(int i=0;i<n;i++)
+        cin>>a[i];
+  
     int res=b;
     for(int i=0;i<n;i++)
     {
         int c=b%a[i];
         for(int j=i+1;j<n;j++)
         {
-            res=max(res,(b/a[i])*a[j]+c);
+            res=max(res, (b/a[i])*a[j]+c);
         }
     }
     cout<<res;
