4229880170d4e96bea44a34832b4d5cc0bfd1152
Srishi__1803 <65821403+ss-shrishi2000@users.noreply.github.com>
2022-07-16 23:03:12 +0530

    Update and rename FleaTravel.cpp to Flea_Travel.cpp

--- Flea_Travel.cpp
+++ Flea_Travel.cpp
@@ -2,12 +2,14 @@
 using namespace std;
 int main()
 {
-    int n;cin>>n;
+    int n;
+    cin>>n;
     while(n%2==0)
     {
         n/=2;
     }
-    if(n==1)cout<<"YES";
+    if(n==1)
+    cout<<"YES";
     else cout<<"NO";
     return 0;
 }
