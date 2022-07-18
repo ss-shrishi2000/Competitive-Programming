b126516f1745381b10534495b08ebd3a84de6ecc
Srishi__1803 <65821403+ss-shrishi2000@users.noreply.github.com>
2022-07-18 23:55:39 +0530

    Update and rename LuckyTickets.cpp to Lucky_Tickets.cpp

--- Lucky_Tickets.cpp
+++ Lucky_Tickets.cpp
@@ -2,14 +2,18 @@
 using namespace std;
 int main()
 {
-   int n,x;cin>>n;
+   int n,x;
+   cin>>n;
    int r0=0,r2=0,r1=0;
    for(int i=0;i<n;i++)
    {
        cin>>x;
-       if(x%3==0)r0++;
-       else if(x%3==1)r1++;
-       else r2++;
+       if(x%3==0)
+          r0++;
+       else if(x%3==1)
+          r1++;
+       else
+          r2++;
    }
    cout<<r0/2+min(r1,r2);
 }
