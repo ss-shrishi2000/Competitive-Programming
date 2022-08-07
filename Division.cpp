a0a312496d423270f557b35afd55e02ba26dbb7d
Srishi__1803 <65821403+ss-shrishi2000@users.noreply.github.com>
2022-08-07 22:56:17 +0530

    Add files via upload

--- Division.cpp
+++ Division.cpp
@@ -1,36 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define ll long long int
-ll p,q;
-
-ll solve(ll d)
-{
-    ll s=p;
-    while(s%q==0)s/=d;
-    return s;
-}
-ll div(ll n)
-{
-    ll ans=solve(n);
-    for(ll i=2;i*i<=n ;++i)
-    {
-        if(n%i==0)
-        {
-            ans=max({ans,solve(i),solve(n/i)});
-        }
-    }
-    return ans;
-}
 int main()
 {
-    int t;cin>>t;
+    int t;
+    cin>>t;
     while(t--)
     {
-        cin>>p>>q;
-        if(p%q)
-            cout<<p<<endl;
-        else
-           cout<<div(q)<<endl;
+        int n;
+        cin>>n;
+        if(n<=1399)
+            cout<<"Division 4"<<endl;
+        else if(n>=1400 && n<=1599)
+            cout<<"Division 3"<<endl;
+        else if(n>=1600 && n<=1899)
+            cout<<"Division 2"<<endl;
+        else if(n>=1900)
+            cout<<"Division 1"<<endl;
     }
     return 0;
 }
