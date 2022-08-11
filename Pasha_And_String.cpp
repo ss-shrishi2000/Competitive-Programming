c43346a14896a8005edcce62c8ee349d2d69fdb4
Srishi__1803 <65821403+ss-shrishi2000@users.noreply.github.com>
2022-08-11 23:53:08 +0530

    Update and rename PashaAndString.cpp to Pasha_And_String.cpp

--- Pasha_And_String.cpp
+++ Pasha_And_String.cpp
@@ -2,17 +2,22 @@
 using namespace std;
 int main()
 {
-    string s;cin>>s;
-    int m;cin>>m;
+    string s;
+    cin>>s;
+    int m;
+    cin>>m;
     int a[200005],x,p=s.size();
     for(int i=0;i<m;i++)
     {
-        cin>>x;a[x-1]++;
+        cin>>x;
+        a[x-1]++;
     }
     for(int i=0;i<p/2;i++)
     {
         if(i!=0)a[i]+=a[i-1];
-            if(a[i]%2!=0)swap(s[i],s[p-i-1]);
+            if(a[i]%2!=0)
+                swap(s[i],s[p-i-1]);
     }
     cout<<s;
+    return 0;
 }
