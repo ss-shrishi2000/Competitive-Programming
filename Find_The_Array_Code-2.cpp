785d7c5c60ad16ae9e2180139d10965fe0f935fc
Srishi__1803 <65821403+ss-shrishi2000@users.noreply.github.com>
2022-08-13 23:23:42 +0530

    Rename Find_The_Array.cpp to Find_The_Array_Code-2.cpp

--- Find_The_Array_Code-2.cpp
+++ Find_The_Array_Code-2.cpp
@@ -9,7 +9,8 @@ int main()
         long long int n;
         cin>>n;
         long long int a[n],b[n];
-        for(int i=0;i<n;i++)cin>>a[i];
+        for(int i=0;i<n;i++)
+           cin>>a[i];
         b[0]=a[0];
         for(int i=1;i<n;i++)
         {
