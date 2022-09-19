e1887b6c97784e439674510fc8fb3e85d1571f9a
Srishi__1803 <65821403+ss-shrishi2000@users.noreply.github.com>
2022-09-19 23:22:36 +0530

    Update and rename MinimumLoss.cpp to Minimum_Loss.cpp

--- Minimum_Loss.cpp
+++ Minimum_Loss.cpp
@@ -2,9 +2,14 @@
 using namespace std;
 int main()
 {
-    int n;cin>>n;
+    int n;
+    cin>>n;
     vector<pair<int,int>> p(n);
-    for(int i=0;i<n;i++){cin>>p[i].first;p[i].second=i;}
+    for(int i=0;i<n;i++)
+    {  
+      cin>>p[i].first;
+      p[i].second=i;
+    }
     sort(p.begin(),p.end());
     long minval = 0;
     for (int i=1;i<n;i++) {
