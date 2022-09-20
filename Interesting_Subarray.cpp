a63a76feb5b2e7fd177690535daf4b73d5318f18
Srishi__1803 <65821403+ss-shrishi2000@users.noreply.github.com>
2022-09-20 23:46:57 +0530

    Update and rename InterestingSubarray.cpp to Interesting_Subarray.cpp

--- Interesting_Subarray.cpp
+++ Interesting_Subarray.cpp
@@ -2,10 +2,12 @@
 using namespace std;
 int main()
 {
-    int t;cin>>t;
+    int t;
+    cin>>t;
     while(t--)
     {
-        int n;cin>>n;
+        int n;
+        cin>>n;
         int a[n];
         for(int i=0;i<n;i++)cin>>a[i];
         int l=-1,r=-1;
