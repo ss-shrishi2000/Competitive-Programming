6bbc1f64f2d05d94c6f2bad8854ae4f6168380b2
Srishi__1803 <65821403+ss-shrishi2000@users.noreply.github.com>
2022-12-02 00:48:26 +0530

    Update and rename I'mBoredOfLife.cpp to I'm_Bored_Of_Life.cpp

--- I'm_Bored_Of_Life.cpp
+++ I'm_Bored_Of_Life.cpp
@@ -4,8 +4,10 @@ int main()
 {
     long long int a,b,p=1;
     cin>>a>>b;
-    long long int c=min(a,b);
-    for(int i=2;i<=c;i++)p*=i;
+    long long int c = min(a,b);
+    for(int i=2;i<=c;i++)
+        p*=i;
+    
     cout<<p<<"\n";
     return 0;
 }
