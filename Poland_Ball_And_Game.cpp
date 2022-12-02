574636e202c040c0ebd3ad6e8bfe47e685d53c40
Srishi__1803 <65821403+ss-shrishi2000@users.noreply.github.com>
2022-12-03 01:02:53 +0530

    Update and rename PolandBallAndGame.cpp to Poland_Ball_And_Game.cpp

--- Poland_Ball_And_Game.cpp
+++ Poland_Ball_And_Game.cpp
@@ -2,7 +2,8 @@
 using namespace std;
 int main()
 {
-    int n,m,p=0;cin>>n>>m;
+    int n,m,p=0;
+    cin>>n>>m;
     set<string> s;
     string str;
     for(int i=0;i<n;i++)
@@ -21,5 +22,6 @@ int main()
         cout<<"YES";
     else
         cout<<"NO";
+    
     return 0;
 }
