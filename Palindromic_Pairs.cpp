28ec281c48efad28e2bfe57855e8065a144ab92a
Srishi__1803 <65821403+ss-shrishi2000@users.noreply.github.com>
2022-12-03 01:05:02 +0530

    Update and rename PalindromicPairs.cpp to Palindromic_Pairs.cpp

---
+++ Palindromic_Pairs.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int left[3000],right[3000];
+    string s;
+    cin>>s;
+    int n=s.size(),l,r;
+    
+    for(int i=0;i<n;i++)
+    {
+        for(l=i,r=i;l>=0 && (r<n) && (s[l]==s[r]);l--,r++)
+        {
+            right[l]++;
+            left[r]++;
+        }
+        for(l=i,r=i+1; l>=0 && (r<n) && (s[l]==s[r]);l--,r++)
+        {
+            right[l]++;
+            left[r]++;
+        }
+    }
+    long long res=0;
+    for(int i=1;i<n;i++)
+        left[i]+=left[i-1];
+    
+    for(int i=0;i<n-1;i++)
+        res+=left[i]*right[i+1];
+    
+    cout<<res<<endl;
+}
