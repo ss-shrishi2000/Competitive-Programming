cd6e7551a4e73d5e9623634e51d607037e89142f
Srishi__1803 <65821403+ss-shrishi2000@users.noreply.github.com>
2022-12-08 23:40:56 +0530

    Update and rename PrimitivePrimes.cpp to Primitive_Primes.cpp

--- Primitive_Primes.cpp
+++ Primitive_Primes.cpp
@@ -4,8 +4,8 @@ using namespace std;
 int main()
 {
     ios_base::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
+    cin.tie(0);
+    cout.tie(0);
 
     lon n,m,p,ans1,ans2;
     cin>>n>>m>>p;
