269c4691d6141e41bfb22e189a44a94b70047998
Srishi__1803 <65821403+ss-shrishi2000@users.noreply.github.com>
2022-12-21 23:49:29 +0530

    Update and rename ProductOfThreeNumbers.cpp to Product_Of_Three_Numbers.cpp

--- Product_Of_Three_Numbers.cpp
+++ Product_Of_Three_Numbers.cpp
@@ -5,7 +5,8 @@ int main()
     int t;cin>>t;
     while(t--)
     {
-        int n;cin>>n;
+        int n;
+        cin>>n;
         int a[3]{0},j=0;
         for(int i=2;i*i<=n&&(j<2);i++)
         {
@@ -13,8 +14,11 @@ int main()
             n/=i;
             }
         }
-         if(j!=2)cout<<"NO"<<endl;
-         else if(a[1]!=n)cout<<"YES"<<endl<<a[0]<<" "<<a[1]<<" "<<n<<endl;
-         else cout<<"NO"<<endl;
+         if(j!=2)
+             cout<<"NO"<<endl;
+         else if(a[1]!=n)
+             cout<<"YES"<<endl<<a[0]<<" "<<a[1]<<" "<<n<<endl;
+         else 
+             cout<<"NO"<<endl;
     }
 }
