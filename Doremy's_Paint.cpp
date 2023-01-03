0119dec9d5f53295fc69e72e366059158bbb47e9
Shrishti <65821403+ss-shrishi2000@users.noreply.github.com>
2023-01-04 01:44:46 +0530

    Add files via upload

---
+++ Doremy's_Paint.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int x;
+        for(int i = 0; i < n; i++)cin>>x;
+        cout<<1<<" "<<n<<endl;
+    }
+    return 0;
+}
