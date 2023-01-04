379db77e2e523523a3af4957c66d63577ec4d0e4
Shrishti <65821403+ss-shrishi2000@users.noreply.github.com>
2023-01-05 01:42:11 +0530

    Update MultiplesOfLength.cpp

--- MultiplesOfLength.cpp
+++ MultiplesOfLength.cpp
@@ -2,9 +2,12 @@
 using namespace std;
 int main()
 {
-    long long int n;cin>>n;
+    long long int n;
+    cin>>n;
     long long int a[n];
-    for(int i=0;i<n;i++)cin>>a[i];
+    for(int i=0;i<n;i++)
+        cin>>a[i];
+    
     if(n==1)
     {
         for(int k=1;k<=2;k++)
@@ -14,7 +17,8 @@ int main()
         cout<<1<<" "<<1<<endl;
         cout<<-a[0]<<endl;
     }
-    else{
+    else
+    {
         cout<<1<<" "<<1<<endl<<-a[0]<<endl;
         cout<<1<<" "<<n<<endl;
         cout<<0<<" ";
