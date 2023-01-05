07e6d9fdfdd2f7514888bb473dfbbce14c9cea9e
Shrishti <65821403+ss-shrishi2000@users.noreply.github.com>
2023-01-05 22:30:09 +0530

    Update and rename ReapeatingCipher.cpp to Reapeating_Cipher.cpp

--- Reapeating_Cipher.cpp
+++ Reapeating_Cipher.cpp
@@ -2,13 +2,16 @@
 using namespace std;
 int main()
 {
-    int n;cin>>n;
-string s,s1;cin>>s;
-int j=0;
+    int n;
+    cin>>n;
+    string s,s1;
+    cin>>s;
+    int j=0;
 for(int i=0;i<s.length();i+=j)
 {
     s1+=s[i];
     j++;
 }
-cout<<s1;
+    cout<<s1;
+    return 0;
 }
