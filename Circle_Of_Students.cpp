bbd73991e60f99712e4570c9d1002c77f8d58b8c
Shrishti <65821403+ss-shrishi2000@users.noreply.github.com>
2023-01-24 22:53:54 +0530

    Update Circle_Of_Students.cpp

--- Circle_Of_Students.cpp
+++ Circle_Of_Students.cpp
@@ -10,7 +10,15 @@ int main()
         int f[n];f[0]=1;
         bool flag=true;
         for(int i=1;i<n;i++)f[i]=abs(a[i]-a[i-1]);
-        for(int i=0;i<n;i++){if(f[i]!=1&&(f[i]!=n-1)){cout<<"NO"<<endl;flag=false;break;}}
+        for(int i=0;i<n;i++)
+        {
+        if(f[i]!=1&&(f[i]!=n-1))
+        {
+            cout<<"NO"<<endl;
+            flag=false;
+            break;
+            }
+        }
         if(flag!=false)cout<<"YES"<<endl;
     }
 }
